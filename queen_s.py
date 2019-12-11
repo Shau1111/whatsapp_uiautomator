a2c8c9f0c314f6d0cc9a6fdee19386044a08c235
Abhishek Gaurav <11abhishek11@gmail.com>
2019-12-11 15:46:03 +0530

    readLastMessage added.

--- queen_s.py
+++ queen_s.py
@@ -10,30 +10,47 @@ center = [500, 800]
 #new branch - abhishek
 
 def getXmlSnapshot():
-    xml = d.dump()
-    message_1=d(resourceId="com.whatsapp:id/single_msg_tv" ).text
-    soup = BeautifulSoup(xml, 'xml')
-    print(message_1)
-    #print(xml)
+  xml = d.dump()
+  # message_1=d(resourceId="com.whatsapp:id/single_msg_tv" ).text
+  soup = BeautifulSoup(xml, 'xml')
+  # print(message_1)
+  print(xml)
 
 # swiping functions
 def fullPageSwipe():
-    d.swipe(100, 1400, 100, 150, steps=50)
+  d.swipe(100, 1400, 100, 150, steps=50)
 
 def miniSwipe():
-    d.swipe(100, 1000, 100, 600, steps=50)
-getXmlSnapshot()
-#def downloadImage():
- #   message_1=d(resourceId="com.whatsapp:id/single_msg_tv" ).text
-    #os.system("adb shell input text " + url)
-  #  print(message_1)
-    #d.press.enter()
-    #time.sleep(3)
-    #d.long_click(*center)
-    # d(text='Download image').click()
-    #d(text='Save image').click()
+  d.swipe(100, 1000, 100, 600, steps=50)
 
+def downloadImage():
+  message_1=d(resourceId="com.whatsapp:id/single_msg_tv" ).text
+  os.system("adb shell input text " + url)
+  print(message_1)
+  d.press.enter()
+  time.sleep(3)
+  d.long_click(*center)
+  d(text='Download image').click()
+  d(text='Save image').click()
 
+def readLastMsgFromWhatsApp():
+  messageElement = d(resourceId='com.whatsapp:id/message_text')
+  message = "none"
+  while (messageElement and messageElement.exists):
+    message = messageElement.text
+    print(message)
+    messageElement = messageElement.down(resourceId='com.whatsapp:id/message_text')
+    # break
+  
+  print("last_message: ", message)
+  return message
+
+def sendMessageOnWhatsApp(msg):
+  d(resourceId='com.whatsapp:id/entry').set_text(msg)
+  d(resourceId='com.whatsapp:id/send').click()
+
+# sendMessageOnWhatsApp(readMsgFromWhatsApp())
+readLastMsgFromWhatsApp()
 #url = "https://s3.ap-south-1.amazonaws.com/static.queensapp.in/queensSmall.png"
 #downloadImage()
 #getXmlSnapshot()
